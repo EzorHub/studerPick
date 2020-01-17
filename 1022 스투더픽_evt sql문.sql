3679f0b27a79d3c50f80d3bcba8e30e272fd1094
EzorHub <zozo.park@kakao.com>
2020-01-18 02:15:48 +0900

    Upload files

---
+++ 1022 스투더픽_evt sql문.sql
@@ -0,0 +1,30 @@
+--------------------------------------------------------
+--  파일이 생성됨 - 화요일-10월-22-2019   
+--------------------------------------------------------
+REM INSERTING into EVT
+SET DEFINE OFF;
+Insert into EVT (EVT_NO,EVT_TITLE,EVT_CONTENT,DISCOUNT,EVT_START,EVT_END,MAN_NO) values (1,'스투더픽 홈페이지 오픈이벤트','페이지 오픈기념
+이용카페의 이용권 1000원 쿠폰',1000,to_date('19/07/14','RR/MM/DD'),to_date('19/08/14','RR/MM/DD'),1);
+Insert into EVT (EVT_NO,EVT_TITLE,EVT_CONTENT,DISCOUNT,EVT_START,EVT_END,MAN_NO) values (2,'이 구역 리뷰왕 이벤트','카페 이용후기를 남긴 피커중 추첨을 통해
+이용권 1000원 쿠폰',1000,to_date('19/07/04','RR/MM/DD'),to_date('19/08/04','RR/MM/DD'),1);
+Insert into EVT (EVT_NO,EVT_TITLE,EVT_CONTENT,DISCOUNT,EVT_START,EVT_END,MAN_NO) values (3,'줄서서 먹던 맛집후기 이벤트','맛집 이용후기를 남긴 피커중 선착순
+음식점 할인권 1000원 쿠폰',1000,to_date('19/07/31','RR/MM/DD'),to_date('19/08/31','RR/MM/DD'),1);
+Insert into EVT (EVT_NO,EVT_TITLE,EVT_CONTENT,DISCOUNT,EVT_START,EVT_END,MAN_NO) values (4,'카페찾고 결제까지 10초면끗','스투더페이를 이용한 피커에게
+1500원 할인 쿠폰',1500,to_date('19/07/02','RR/MM/DD'),to_date('19/09/02','RR/MM/DD'),1);
+Insert into EVT (EVT_NO,EVT_TITLE,EVT_CONTENT,DISCOUNT,EVT_START,EVT_END,MAN_NO) values (5,'스투더모임 작명 이벤트','신박한 스투더 모임이름을 짓는 튜터에게 추첨을 통해
+카페 예약 할인 15000원 쿠폰',15000,to_date('19/06/14','RR/MM/DD'),to_date('19/09/14','RR/MM/DD'),2);
+Insert into EVT (EVT_NO,EVT_TITLE,EVT_CONTENT,DISCOUNT,EVT_START,EVT_END,MAN_NO) values (6,'너가찾는 바로그거 스투더에 다~있다','sns에 스투더 홍보용 댓글 및 영상을 올린 고객에게
+추첨을 통해 15000원 쿠폰지급',null,to_date('19/07/24','RR/MM/DD'),to_date('19/08/24','RR/MM/DD'),2);
+Insert into EVT (EVT_NO,EVT_TITLE,EVT_CONTENT,DISCOUNT,EVT_START,EVT_END,MAN_NO) values (7,'카페찾아찾아 이벤트','스터디카페 사장님과 스투더를 연결해준 고객에게
+카페 예약 할인권 20000원 쿠폰',null,to_date('19/05/30','RR/MM/DD'),to_date('19/07/28','RR/MM/DD'),2);
+Insert into EVT (EVT_NO,EVT_TITLE,EVT_CONTENT,DISCOUNT,EVT_START,EVT_END,MAN_NO) values (8,'스투더 성실모임 이벤트','스투더 모임에 대한 인증 및 재미있는 스토리를 남긴
+고객에게 추첨을 통해 이용권 1500원 쿠폰',null,to_date('19/07/02','RR/MM/DD'),to_date('19/08/12','RR/MM/DD'),2);
+Insert into EVT (EVT_NO,EVT_TITLE,EVT_CONTENT,DISCOUNT,EVT_START,EVT_END,MAN_NO) values (9,'스투더 목표달성 이벤트','스투더를 통해 목표달성하여 후기를 남긴 피커중
+추첨을 통해 이용권 20000원 쿠폰',null,to_date('19/07/01','RR/MM/DD'),to_date('19/09/01','RR/MM/DD'),1);
+Insert into EVT (EVT_NO,EVT_TITLE,EVT_CONTENT,DISCOUNT,EVT_START,EVT_END,MAN_NO) values (10,'처음이용 이벤트','스투더 사이트를 처음이용한 고객에게
+이용권 1000원 할인 쿠폰',null,to_date('19/07/14','RR/MM/DD'),to_date('19/11/14','RR/MM/DD'),1);
+Insert into EVT (EVT_NO,EVT_TITLE,EVT_CONTENT,DISCOUNT,EVT_START,EVT_END,MAN_NO) values (11,'소심 이벤트','날마다 오는 이벤트가 아닙니다. 관리자가 휴가가서 만든 이벤트. 100원 할인',100,to_date('19/08/01','RR/MM/DD'),to_date('19/08/14','RR/MM/DD'),1);
+Insert into EVT (EVT_NO,EVT_TITLE,EVT_CONTENT,DISCOUNT,EVT_START,EVT_END,MAN_NO) values (12,'소심 이벤트2','날마다 오는 이벤트가 아닙니다. 관리자가 휴가가서 만든 이벤트. 200원 할인',200,to_date('19/08/02','RR/MM/DD'),to_date('19/08/15','RR/MM/DD'),1);
+Insert into EVT (EVT_NO,EVT_TITLE,EVT_CONTENT,DISCOUNT,EVT_START,EVT_END,MAN_NO) values (13,'소심 이벤트3','날마다 오는 이벤트가 아닙니다. 관리자가 휴가가서 만든 이벤트. 300원 할인',300,to_date('19/08/03','RR/MM/DD'),to_date('19/08/16','RR/MM/DD'),1);
+Insert into EVT (EVT_NO,EVT_TITLE,EVT_CONTENT,DISCOUNT,EVT_START,EVT_END,MAN_NO) values (14,'소심 이벤트4','날마다 오는 이벤트가 아닙니다. 관리자가 휴가가서 만든 이벤트. 400원 할인',400,to_date('19/08/04','RR/MM/DD'),to_date('19/08/17','RR/MM/DD'),1);
+Insert into EVT (EVT_NO,EVT_TITLE,EVT_CONTENT,DISCOUNT,EVT_START,EVT_END,MAN_NO) values (15,'소심 이벤트5','날마다 오는 이벤트가 아닙니다. 관리자가 휴가가서 만든 이벤트. 500원 할인',500,to_date('19/08/05','RR/MM/DD'),to_date('19/08/18','RR/MM/DD'),1);
